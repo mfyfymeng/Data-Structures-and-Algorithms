21b9ce03a25be3b87e6e4f56508662d2f3d8e040
mfyfymeng <mfyfymeng@163.com>
2020-02-25 22:15:04 +0800

    first commit

---
+++ 链表/src/cn/sia/Main.java
@@ -0,0 +1,8 @@
+package cn.sia;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+    }
+}
