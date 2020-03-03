6d1e5ce6f945e6d0b977ab93d4c04fa49efae9cc
mfyfymeng <mfyfymeng@163.com>
2020-03-03 21:08:15 +0800

    first commit

--- 02-链表/src/cn/sia/Main.java
+++ 02-链表/src/cn/sia/Main.java
@@ -2,14 +2,22 @@ package cn.sia;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList2<>();
+//        List<Integer> list = new LinkedList2<>();
+//
+//        list.add(20);
+//        list.add(0, 10);
+//        list.add(30);
+//        list.add(list.size(), 40);
+//
+//        list.remove(1);
 
-        list.add(20);
-        list.add(0, 10);
-        list.add(30);
-        list.add(list.size(), 40);
-
-        list.remove(1);
+        List<Integer> list = new ArrayList2<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < 50; i++) {
+            list.remove(0);
+        }
 
         System.out.println(list);
     }
